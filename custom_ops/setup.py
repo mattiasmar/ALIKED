851a7edacf6bd89ede4014b03d7dd51924fb4227
Maxim Vafin <maxim.vafin@intel.com>
2023-10-26 21:46:33 +0200

    Update setup.py for cpu

--- custom_ops/setup.py
+++ custom_ops/setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CppExtension
+import torch
 
 
-setup(name='custom_ops', 
-      ext_modules=[CUDAExtension('get_patches', ['get_patches.cpp', 'get_patches_cuda.cu'])], 
+if torch.cuda.is_available():
+    extension = CUDAExtension('get_patches',
+                              ['get_patches.cpp', 'get_patches_cuda.cu'])
+else:
+    extension = CppExtension('get_patches', ['get_patches.cpp'])
+setup(name='custom_ops',
+      ext_modules=[extension],
       cmdclass={'build_ext': BuildExtension})
